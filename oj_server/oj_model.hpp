9d1d581535a20ddc46c77f4176d0210e9f7b64a3
FelixYuanSy <ysy12141314@gmail.com>
2025-10-28 10:36:58 +0800

    完成获取题目

--- oj_server/oj_model.hpp
+++ oj_server/oj_model.hpp
@@ -29,6 +29,9 @@ namespace ns_model
     const std::string questions_path = "./questions/";
     class Model
     {
+    private:
+        std::unordered_map<std::string, Question> questions;
+
     public:
         Model()
         {
@@ -47,15 +50,14 @@ namespace ns_model
             while (getline(in, line))
             {
                 std::vector<std::string> tokens;
-                //切割字符串line后存入tokens
-                
+                // 切割字符串line后存入tokens
 
-                if(tokens.size() != 5)
+                if (tokens.size() != 5)
                 {
-                    LOG(WARNING) <<"加载个别题目失败,检查文件"<<std::endl;
+                    LOG(WARNING) << "加载个别题目失败,检查文件" << std::endl;
                     continue;
                 }
-                //存入Question结构体
+                // 存入Question结构体
                 Question q;
                 q.number = tokens[0];
                 q.name = tokens[1];
@@ -63,23 +65,44 @@ namespace ns_model
                 q.cpu_limit = atoi(tokens[3].c_str());
                 q.mem_limit = atoi(tokens[4].c_str());
 
-                std::string path =" ./questions/";
+                std::string path = " ./questions/";
                 path += q.number;
                 path += "/";
 
-                FileUtil::ReadFile(path+"desc.txt",&q.desc,true);
-                FileUtil::ReadFile(path+"header.cpp",&q.header,true);
-                FileUtil::ReadFile(path+"tail.cpp",&q.tail,true);
+                FileUtil::ReadFile(path + "desc.txt", &q.desc, true);
+                FileUtil::ReadFile(path + "header.cpp", &q.header, true);
+                FileUtil::ReadFile(path + "tail.cpp", &q.tail, true);
 
-                questions.insert({q.number,q});
-
+                questions.insert({q.number, q});
             }
-            LOG(INFO)<<"加载题库成功"<<std::endl;
+            LOG(INFO) << "加载题库成功" << std::endl;
             in.close();
             return true;
         }
+        bool GetAllQuestions(std::vector<Question> *out)
+        {
+            if (questions.size() == 0)
+            {
+                LOG(ERROR) << "用户题库获取失败" << std::endl;
+                return false;
+            }
+            for (const auto &iter : questions)
+            {
+                out->push_back(iter.second);
+            }
+            return true;
+        }
 
-    private:
-        std::unordered_map<std::string, Question> questions;
-    }
+        bool GetQuestion(const std::string &number, Question *q)
+        {
+            const auto &iter = questions.find(number);
+            if (iter == questions.end())
+            {
+                LOG(ERROR) << "用户获取题目:  " << number << "失败" << std::endl;
+                return false;
+            }
+            (*q) = iter->second;
+            return true;
+        }
+    };
 }
