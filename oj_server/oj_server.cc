ca390b4cf83512a37cb298d68c691223ec373f86
FelixYuanSy <ysy12141314@gmail.com>
2025-10-31 01:57:57 +0800

    修正函数缺少ctrl

--- oj_server/oj_server.cc
+++ oj_server/oj_server.cc
@@ -1,10 +1,12 @@
 #include <iostream>
 #include "../comm/httplib.h"
+#include "../comm/util.hpp"
 #include "oj_control.hpp"
 #include "oj_view.hpp"
 
 using namespace httplib;
 using namespace ns_control;
+using namespace ns_util;
 int main()
 {
     Server svr;
@@ -23,12 +25,15 @@ int main()
                 ctrl.Question(number,&html);
                 resp.set_content(html, "text/html; charset=utf-8"); });
     // 判题页面
-    svr.Get(R"(/judge/(\d+))", [](const Request &req, Response &resp)
-            {
+    svr.Post(R"(/judge/(\d+))", [&ctrl](const Request &req, Response &resp)
+             {
+        LOG(INFO) << "收到请求" <<std::endl;
         std::string number = req.matches[1];//用0表示匹配整个地址,1表示捕获第一个捕获组内容(括号里)
-        resp.set_content("指定题目的判题: "+number, "text/html;charset=utf-8"); });
+        std::string result_json;
+        ctrl.Judge(number,req.body,&result_json);
+        resp.set_content(result_json, "application/json;charset=utf-8"); });
 
     svr.set_base_dir("./wwwroot");
-    svr.listen("0.0.0.0", 8089);
+    svr.listen("0.0.0.0", 8080);
     return 0;
 }
